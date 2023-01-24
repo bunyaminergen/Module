06089e48455cab636247ee0353c48e6ea846501f
bunyaminergen <bunyaminergen@yahoo.com.tr>
2023-01-24 03:41:27 +0300

    Initial commit

---
+++ be.py
@@ -0,0 +1,159 @@
+
+
+
+"""
+def imp_lib():
+"""
+
+import numpy as np
+import pandas as pd
+import researchpy as rp
+
+#
+pd.set_option('display.max_columns', None)
+#
+pd.set_option('display.max_rows', None)
+#
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+#
+pd.options.mode.chained_assignment = None
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format',lambda x:'%.2f' % x)
+pd.set_option('display.width',1000)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.max_seq_items', None)
+pd.set_option('max_colwidth', None)
+pd.set_option('display.colheader_justify', 'right')
+pd.set_option('display.html.table_schema', True)
+pd.set_option('display.unicode.east_asian_width', True)
+
+# EXCEL FILES
+def read_all_excel_files(filename:str) -> list:
+
+    from os import listdir
+
+    files = listdir(filename)
+
+    file_list = []
+
+    for i in files:
+        if "xlsx" and "xls" in i:
+            file_list.append(i)
+
+    return file_list
+
+def read_all_excel_sheets(filename: str) -> print:
+
+    for i in pd.ExcelFile(filename).sheet_names:
+        print("Sheet Name: " + i)
+        print("#" * 100)
+        print(pd.read_excel(filename, sheet_name = i).head())
+        print("#" * 100)
+
+    # file_list = read_all_excel_files(r"C:\Users\bunya\Desktop\sun4tech\HEIMDALL")
+
+    # for i in file_list:
+    # print("\n" * 2)
+    # print("#" * 100)
+    # print("File Name: " + i)
+    # print("#" * 100)
+    # read_all_sheets(i)
+
+
+# https: // stackoverflow.com / questions / 17977540 / pandas - looking - up - the - list - of - sheets - in -an - excel - file
+
+# xl = pd.ExcelFile('foo.xls')
+
+# xl.sheet_names  # see all sheet names
+
+# xl.parse("sheet_name")  # read a specific sheet to DataFrame
+
+# CSV FILES
+def read_all_csv_files(filename:str) -> list:
+
+    from os import listdir
+
+    files = listdir(filename)
+
+    file_list = []
+
+    for i in files:
+        if "csv" in i:
+            file_list.append(i)
+
+    return file_list
+
+# Data info
+
+def data_info(data, head = 5, tail = 5):
+
+    print("\n", "#" * 25, "Head" , "#" *25)
+    print(data.head(head), "\n")
+
+    print("#" * 25, "Tail" , "#" *25)
+    print(data.tail(tail), "\n")
+
+    print("#" * 25, "Missing Values", "#" *25)
+    print(pd.DataFrame(data.isnull().sum()), "\n")
+
+    print("#" * 25, "Data Types", "#" *25)
+    print(pd.DataFrame(data.dtypes), "\n")
+
+    print("#" * 25, "Shape", "#" *25)
+    print(data.shape, "\n")
+
+    print("#" * 25, "Columns", "#" *25)
+    print(list(data.columns), "\n")
+
+    print("#" * 25, "Categorical Variables Summary", "#" *25)
+    print(rp.summary_cat(data.select_dtypes(exclude=np.number)), "\n")
+
+    print("#" * 25, "Continuous Variables Summary", "#" *25)
+    print(rp.summary_cont(data.select_dtypes(include=np.number)), "\n")
+
+    print("#" * 25, "Quantiles", "#" *25)
+    print(data.quantile([0, 0.05, 0.25, 0.95, 0.99, 1]).T, "\n")
+
+if __name__ == "main":
+
+    # sys.path.insert(0, r"C:\Users\bunya\Desktop\BE_PC\3 -DataScience\1 - Codes\1 - Modül")
+
+    # import be
+
+########################################################################################################################
+# Kategorik verilerin object type'ına çevrilmesi
+########################################################################################################################
+
+    """
+    final_data.select_dtypes(exclude=np.number).head()
+
+    final_data.select_dtypes(include=np.number).head()
+
+    rp.summary_cat(final_data.select_dtypes(exclude=np.number))
+
+    rp.summary_cont(final_data.select_dtypes(include=np.number))
+
+    final_data[["education", "prevalentStroke", "prevalentHyp", "diabetes", "TenYearCHD"]] = final_data[
+        ["education", "prevalentStroke", "prevalentHyp", "diabetes", "TenYearCHD"]].astype("object")
+
+    """
+
+########################################################################################################################
+# Türkçe Karakter
+# UnicodeEncodeError: 'charmap' codec can't encode characters
+########################################################################################################################
+
+"""
+
+UnicodeEncodeError: 'charmap' codec can't encode characters
+
+https://stackoverflow.com/questions/27092833/unicodeencodeerror-charmap-codec-cant-encode-characters
+
+encoding="ISO-8859-1"
+encoding="latin-1"
+encoding="utf-8"
+encoding="utf8"
+
+"""
---
+++ data.py
@@ -0,0 +1,111 @@
+# unstructured class
+
+import numpy as np
+import pandas as pd
+
+"""
+Yapılandırılmamış veri temizliği ile uğraşırken kullanılabilecek fonksiyonları içerir
+"""
+
+def cat_to_var(data, cat_column, var_column, suffix: str = None, prefix: str = None, inplace: bool = False ):
+
+    """
+    # bir değişkenin içindeki kategorik değişkenlerin diğer değişkendeki değerlerini alıp hepsini indexlerine göre
+    # yeni bir kolon oluşturarak o değerleri atar
+    # TESTADI adında bir değişkenimiz olsun içinde kategorik değişkenleri barındıran test isimleri olsun
+    # Bu teslerin değerleri de başka bir değişken içinde olsun
+    # biz de bütün bu TESTADI değişkenindeki bütün değişkenlerin diğer değişkende olan değerlerini
+    # ayrı bir değişken olarak indexlerine göre atamak istiyor olalım.
+
+    # https://stackoverflow.com/questions/48027171/create-a-variable-in-a-pandas-dataframe-based-on-information-in-the-dataframe
+
+    numpy where
+    # https://numpy.org/doc/stable/reference/generated/numpy.where.html
+
+    Test:
+
+    dataframe = {"A": [1,2,1],
+                 "B": ["Z", "X","Z"],
+                 "C": ["A","S","D"]}
+
+    data = pd.DataFrame(dataframe)
+
+    cat_to_var(data,"B","C", suffix="_deneme", inplace=True)
+
+    data.head()
+
+    """
+
+    # Create a copy of the dataframe if inplace is not set
+    if not inplace:
+        data = data.copy()
+
+    # Iterate through unique values in the categorical column
+    for j in data[cat_column].unique():
+        # Create the new column name
+        if suffix:
+            new_col = j + suffix
+        elif prefix:
+            new_col = prefix + j
+        else:
+            new_col = j
+
+        # Assign the values from the variable column to the new column
+        data[new_col] = np.where(data[cat_column] == j, data[var_column], np.NaN)
+
+    return data
+
+def del_repeated_last_occur_str(data, char, column, inplace: bool = False):
+
+    """
+
+    The function is used to remove the last occurrence of a repeated string in a specific column of a DataFrame.
+    If there are two repeated string expressions in the values of a column,
+    the function will remove everything after the last occurrence of the specified character.
+
+    :param data: dataframe as dataframe
+    :param char: want to convert character as string
+    :param column: want to implement apply columns as string
+    :param inplace: same as other libraries
+    :return: return data as dataframe
+
+    Test and Example:
+
+    data = {  "ID" :  [1,1,2,2,3,4,5],
+            "TEST" :  ["1.45","2.45","47","12.12.00","123.00.00","23.45","215.00"]}
+
+    data = pd.DataFrame(data)
+
+    data.head()
+
+    data["TEST"] = data["TEST"].astype(float)
+
+    del_repeated_last_occur_str(data,".","TEST", inplace = True)
+
+    The above code is a function to remove the last occurrence of a repeated string in a specific column of a DataFrame.
+    When you want to convert the 'TEST' column to float, it will give an error because of unwanted characters.
+    If you do not want to replace all the characters with methods like replace(),
+    you can use this function to remove only the characters after the specific one in the column.
+    Note: If you use pd.to_numeric(data["column_name"], errors='coerce') to convert the column to float,
+    it will return 'nan' for invalid values and may cause data loss.
+    This function can be used as a pre-processing step to remove unwanted characters before converting the column to float.
+
+
+    # https://www.w3schools.com/python/ref_string_rfind.asp
+    # https://www.w3schools.com/python/ref_string_rindex.asp
+    # https://docs.python.org/3/library/stdtypes.html#str.rindex:~:text=str.rfind,is%20not%20found.
+    # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.at.html
+
+    """
+    # Create a copy of the dataframe if inplace is not set
+    if not inplace:
+        data = data.copy()
+
+    if data[column].str.contains(char).any():
+        for i in data.index:
+            if data.loc[i, column].count(char) > 1:
+                data.loc[i, column] = data.loc[i, column][:data.loc[i, column].rindex(char)]
+    else:
+        raise ValueError("The character is not present in the column values.")
+
+    return data
